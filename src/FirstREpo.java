d5585c2d04562f6fcf439c4cf1bd39e4e6ef368d
AminHasanov2000 <147754079+AminHasanov2000@users.noreply.github.com>
2023-10-17 19:50:47 -0400

    Update FirstREpo.java

--- src/FirstREpo.java
+++ src/FirstREpo.java
@@ -2,5 +2,7 @@ public class FirstREpo {
     public static void main(String[] args) {
         System.out.println("First repository");
         System.out.println("Hello");
+        System.out.println("First repository");
+        System.out.println("Hello");
     }
 }
